2beed8a1ecb35149ca66a0fbfa0ffe536056ed41
cuijinyu <cuijinyu@cuijinyudeMacBook-Pro.local>
2018-12-18 14:33:31 +0800

    AST初始化

--- src/main.cc
+++ src/main.cc
@@ -3,9 +3,10 @@
 #include "laxical_analyzer.h"
 using namespace std;
 int main () {
-    Laxer laxer("/home/cuijinyu/文档/pl0Compiler/test/test.pl0");
+    Laxer laxer("/Users/cuijinyu/Documents/pl0/test/test.pl0");
     laxer.tokenizer();
     vector <Token *> tokens = laxer.get_tokens();
     Parser parser(tokens);
     parser.parse();
+    parser.get_root() -> print();
 }
